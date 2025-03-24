3d98391c5d47c07ff7db1f82fbe9d19fe4024e6d
Reagan Odhiambo <errorcode.locked@gmail.com>
2025-03-24 09:11:08 +0300

    Add BookingAdmin for managing bookings and update admin registration for Membership and Class

--- Base/admin.py
+++ Base/admin.py
@@ -4,8 +4,9 @@ from .models import *
 class ClassAdmin(admin.ModelAdmin):
     list_display = ["class_name", "trainer_name", "day_of_week", "starting_time", "ending_time"]
     list_filter = ["class_name","day_of_week", "trainer_name"]
-
-
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["client_name", "trainer_name", "class_name"]
+    list_filter = ["client_name", "trainer_name", "class_name"]
 admin.site.register(Booking)
-admin.site.register(Membership)
-admin.site.register(Class)
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Class, ClassAdmin)
