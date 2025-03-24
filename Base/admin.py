488768eac4f9cb8685fe295bd15be6829f5baa94
Reagan Odhiambo <errorcode.locked@gmail.com>
2025-03-24 09:11:29 +0300

    Refactor ClassAdmin and BookingAdmin for improved readability and consistency

--- Base/admin.py
+++ Base/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 class ClassAdmin(admin.ModelAdmin):
-    list_display = ["class_name", "trainer_name", "day_of_week", "starting_time", "ending_time"]
-    list_filter = ["class_name","day_of_week", "trainer_name"]
+    list_display = [
+        "class_name",
+        "trainer_name",
+        "day_of_week",
+        "starting_time",
+        "ending_time",
+    ]
+    list_filter = ["class_name", "day_of_week", "trainer_name"]
+
+
 class BookingAdmin(admin.ModelAdmin):
     list_display = ["client_name", "trainer_name", "class_name"]
     list_filter = ["client_name", "trainer_name", "class_name"]
+
+
 admin.site.register(Booking)
 admin.site.register(Membership, MembershipAdmin)
 admin.site.register(Class, ClassAdmin)
