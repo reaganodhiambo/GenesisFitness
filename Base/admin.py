722e2c416b8bba3bb5f56e9c0c324ff6570e5303
Reagan Odhiambo <errorcode.locked@gmail.com>
2025-03-24 09:08:43 +0300

    Add ClassAdmin for improved class management in admin interface

--- Base/admin.py
+++ Base/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ["class_name", "trainer_name", "day_of_week", "starting_time", "ending_time"]
+    list_filter = ["class_name","day_of_week", "trainer_name"]
 
 
 admin.site.register(Booking)
